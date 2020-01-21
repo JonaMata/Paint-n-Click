6a8ac10b9173144ed60682a2ed614996f14a1af7
jessseee <j.w.visser-3@student.utwente.nl>
2020-01-21 10:59:18 +0100

    Cropped video input to be square before compression

--- opencv_test_video.py
+++ opencv_test_video.py
@@ -5,11 +5,15 @@ cap = cv2.VideoCapture(0)
 
 while True:
 	ret, frame = cap.read()
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	resized = cv2.resize(gray, (28, 28), cv2.INTER_CUBIC)
+	width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+	size_diff = int((width - height)/2)
+	y1, y2, x1, x2 = 0, height, size_diff, width-size_diff
+	crop_frame = frame[y1:y2, x1:x2]
+	gray_frame = cv2.cvtColor(crop_frame, cv2.COLOR_BGR2GRAY)
+	resized_frame = cv2.resize(gray_frame, (28, 28), cv2.INTER_CUBIC)
 
-	cv2.imshow('compressed', resized)
-	cv2.imshow('input', frame)
+	cv2.imshow('cropped', crop_frame)
+	cv2.imshow('compressed', resized_frame)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
