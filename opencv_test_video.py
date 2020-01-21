a984a4570b5f267e2a4ff56da094f67ab4f77c55
Jonathan Matarazzi <git@jonathanm.nl>
2020-01-21 11:38:18 +0100

    Added sketch prediction to video feed

--- opencv_test_video.py
+++ opencv_test_video.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from predictor import predict_drawing
 
 cap = cv2.VideoCapture(0)
 
@@ -10,9 +11,16 @@ while True:
 	y1, y2, x1, x2 = 0, height, size_diff, width-size_diff
 	crop_frame = frame[y1:y2, x1:x2]
 	gray_frame = cv2.cvtColor(crop_frame, cv2.COLOR_BGR2GRAY)
-	resized_frame = cv2.resize(gray_frame, (28, 28), cv2.INTER_CUBIC)
+	resized_frame = cv2.resize(gray_frame, (28, 28), cv2.INTER_NEAREST)
 	preview_frame = cv2.resize(resized_frame, (200, 200), cv2.INTER_NEAREST)
+	prediction = predict_drawing(resized_frame)
 
+	cv2.putText(crop_frame, 'Prediction: '+', '.join(np.flip(prediction)),
+				(0, 20),
+				cv2.FONT_HERSHEY_SIMPLEX,
+				0.5,
+				(255, 255, 255),
+				1)
 	cv2.imshow('cropped', crop_frame)
 	cv2.imshow('compressed', preview_frame)
 
--- predictor.py
+++ predictor.py
@@ -3,24 +3,23 @@ import tensorflow_datasets as tfds
 import cv2
 import numpy as np
 
-SPLIT_WEIGHTS = (8, 1, 1)
-splits = tfds.Split.TRAIN.subsplit(weighted=SPLIT_WEIGHTS)
-
-(raw_train, raw_val, raw_test), metadata = tfds.load('quickdraw_bitmap',
-                                                     split=list(splits),
-                                                     data_dir=".\dataset",
-                                                     as_supervised=True,
-                                                     with_info=True)
+raw_data, metadata = tfds.load('quickdraw_bitmap',
+                               data_dir=".\dataset",
+                               as_supervised=True,
+                               with_info=True)
 
 classes = metadata.features['label'].names
 
+print(classes)
+
 model = keras.models.load_model('quickdraw_model.h5')
 
-image = cv2.imread('test_image.bmp')
-grayscale = np.array([cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)])
 
-grayscale = grayscale.reshape(grayscale.shape[0], 28, 28, 1)
-
-prediction = model.predict_classes(grayscale)
-
-print(classes[prediction[0]])
+def predict_drawing(image):
+    processed = image.reshape(28, 28, 1)
+    input = np.array([processed])
+    prediction = model.predict_classes(input)
+    extra_prediction = model.predict(input)[0]
+    top_values_index = sorted(range(len(extra_prediction)), key=lambda i: extra_prediction[i])[-5:]
+    predicted_classes = [classes[i]+':'+str(extra_prediction[i]) for i in top_values_index]
+    return predicted_classes
