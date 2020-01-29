c80d72dff37b04dc95050f6179a3d343efc0ab44
Jonathan Matarazzi <git@jonathanm.nl>
2020-01-29 11:25:09 +0100

    Added floor neighbour detection for setting sprite to maze

--- Maze.py
+++ Maze.py
@@ -14,13 +14,13 @@ class Maze(object):
         spritesheet = SpriteSheet("assets/png/tilemap.png")
         self.sprites = {
             Tile.TYPE_WALL: spritesheet.image_at((64, 48, 16, 16)),
-            Tile.TYPE_EMPTY: spritesheet.image_at((48, 48, 16, 16))
+            Tile.TYPE_FLOOR: spritesheet.image_at((48, 48, 16, 16))
         }
         self.maze_sprite_list = pygame.sprite.Group()
         self.width = width
         self.height = height
         self.tile_scale = (size[0] / width / 16)
-        self.grid = [[Tile(x, y, Tile.TYPE_WALL, spritesheet, self.maze_sprite_list, self.tile_scale) for y in range(self.height)] for x in range(self.width)]
+        self.grid = [[Tile(x, y, spritesheet, self.maze_sprite_list, self.tile_scale) for y in range(self.height)] for x in range(self.width)]
         self.start = (2*int(random.uniform(0, self.width//2))+1, 0)
         self.end = (2*int(random.uniform(0, self.width//2))+1, height-1)
         self.set_neighbours()
@@ -28,30 +28,50 @@ class Maze(object):
         self.render_console()
 
     def set_neighbours(self):
+        # Set step_neighbours
         for x in range(1, self.width, 2):
             for y in range(1, self.height, 2):
                 grid_tile = self.grid[x][y]
                 if y > 1:
-                    grid_tile.neighbours['up'] = (self.grid[x][y-1], self.grid[x][y-2])
+                    grid_tile.step_neighbours[Tile.DIRECTION_UP] = (self.grid[x][y - 1], self.grid[x][y - 2])
                 if x < self.width-2:
-                    grid_tile.neighbours['right'] = (self.grid[x+1][y], self.grid[x+2][y])
+                    grid_tile.step_neighbours[Tile.DIRECTION_RIGHT] = (self.grid[x + 1][y], self.grid[x + 2][y])
                 if y < self.height-2:
-                    grid_tile.neighbours['down'] = (self.grid[x][y+1], self.grid[x][y+2])
+                    grid_tile.step_neighbours[Tile.DIRECTION_DOWN] = (self.grid[x][y + 1], self.grid[x][y + 2])
                 if x > 1:
-                    grid_tile.neighbours['left'] = (self.grid[x-1][y], self.grid[x-2][y])
+                    grid_tile.step_neighbours[Tile.DIRECTION_LEFT] = (self.grid[x - 1][y], self.grid[x - 2][y])
+
+        # Set direct neighbours
+        for x in range(self.width):
+            for y in range(self.height):
+                grid_tile = self.grid[x][y]
+                if y > 0:
+                    grid_tile.direct_neighbours[Tile.DIRECTION_UP] = self.grid[x][y-1]
+                if x < self.width-1:
+                    grid_tile.direct_neighbours[Tile.DIRECTION_RIGHT] = self.grid[x+1][y]
+                if y < self.height-1:
+                    grid_tile.direct_neighbours[Tile.DIRECTION_DOWN] = self.grid[x][y+1]
+                if x > 1:
+                    grid_tile.direct_neighbours[Tile.DIRECTION_LEFT] = self.grid[x-1][y]
 
     def generate_maze(self):
-        self.grid[self.start[0]][self.start[1]].update_type(Tile.TYPE_EMPTY)
-        neighbours = self.grid[self.start[0]][1].possible_neighbours()
+        self.grid[self.start[0]][self.start[1]].update_type(Tile.TYPE_START)
+        self.grid[self.end[0]][self.end[1]].update_type(Tile.TYPE_END)
+        neighbours = self.grid[self.start[0]][1].possible_stop_neighbours()
         random.shuffle(neighbours)
         for neighbour in neighbours:
             if neighbour[1].tile_type is Tile.TYPE_WALL:
                 self.do_step(neighbour)
 
+        # Set tile sprites
+        for row in self.grid:
+            for tile in row:
+                tile.set_sprite()
+
     def do_step(self, tile):
-        tile[0].update_type(Tile.TYPE_EMPTY)
-        tile[1].update_type(Tile.TYPE_EMPTY)
-        neighbours = tile[1].possible_neighbours()
+        tile[0].update_type(Tile.TYPE_FLOOR)
+        tile[1].update_type(Tile.TYPE_FLOOR)
+        neighbours = tile[1].possible_stop_neighbours()
         random.shuffle(neighbours)
         if len(neighbours) is 0:
             return
--- Tile.py
+++ Tile.py
@@ -3,36 +3,72 @@ from Spritesheet import Sprite
 
 
 class Tile(object):
-    TYPE_EMPTY = ' '
+    TYPE_FLOOR = ' '
     TYPE_WALL = '#'
     TYPE_DOOR = '|'
+    TYPE_START = 'S'
+    TYPE_END = 'E'
 
-    def __init__(self, x, y, tile_type, spritesheet, sprite_group, tile_scale):
-        self.sprites = {
-            Tile.TYPE_WALL: Sprite(spritesheet, (64, 48, 16, 16), scale=tile_scale),
-            Tile.TYPE_EMPTY: Sprite(spritesheet, (64, 0, 16, 16), scale=tile_scale)
+    DIRECTION_UP = 0
+    DIRECTION_RIGHT = 90
+    DIRECTION_DOWN = 180
+    DIRECTION_LEFT = 270
+
+    def __init__(self, x, y, spritesheet, sprite_group, tile_scale):
+        self.wall_sprite = Sprite(spritesheet, (64, 48, 16, 16), scale=tile_scale)
+        self.floor_sprites = {
+            0: Sprite(spritesheet, (64, 0, 16, 16), scale=tile_scale),
+            1: Sprite(spritesheet, (64, 0, 16, 16), scale=tile_scale),
+            2: Sprite(spritesheet, (64, 0, 16, 16), scale=tile_scale),
+            3: Sprite(spritesheet, (64, 0, 16, 16), scale=tile_scale),
+            4: Sprite(spritesheet, (64, 0, 16, 16), scale=tile_scale)
         }
         self.x = x
         self.y = y
         self.sprite_group = sprite_group
-        self.tile_type = tile_type
-        self.sprite = self.sprites[tile_type]
+        self.tile_type = self.TYPE_WALL
+        self.sprite = self.wall_sprite
         self.sprite.set_pos((x*self.sprite.size[0], y*self.sprite.size[1]))
         self.sprite.add(sprite_group)
-        self.neighbours = {}
+        self.step_neighbours = {}
+        self.direct_neighbours = {}
+        self.riddle = None
 
-        if tile_type is self.TYPE_DOOR:
-            self.riddle = Riddle()
-
-    def possible_neighbours(self):
+    def possible_stop_neighbours(self):
         possible = []
-        for neighbour in self.neighbours.values():
+        for neighbour in self.step_neighbours.values():
             if neighbour[1].tile_type is self.TYPE_WALL:
                 possible.append(neighbour)
         return possible
 
+    def set_sprite(self):
+        if self.tile_type is self.TYPE_FLOOR:
+            floor_neighbours = {}
+            for key, neighbour in self.direct_neighbours:
+                if neighbour.tile_type in (self.TYPE_FLOOR, self.TYPE_START, self.TYPE_END):
+                    floor_neighbours[key] = neighbour
+
+            floor_amount = len(floor_neighbours)
+            floor_direction = min(floor_neighbours.keys())
+
+            if floor_amount is 1:
+                # Single floor
+                return
+            elif floor_amount is 2:
+                if sum(floor_neighbours.keys()) in (180, 360):
+                    # Straight line floor
+                    return
+                else:
+                    # Corner floor
+                    return
+            elif floor_amount is 3:
+                # T-split floor
+                return
+            elif floor_amount is 4:
+                # 4-way floor
+                return
+
     def update_type(self, tile_type):
         self.tile_type = tile_type
-        if tile_type is self.TYPE_EMPTY:
+        if tile_type in (self.TYPE_FLOOR, self.TYPE_START, self.TYPE_END):
             self.sprite.remove(self.sprite_group)
-
